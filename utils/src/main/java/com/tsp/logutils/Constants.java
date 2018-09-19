4cef115fe6595426cc041943fad2d6982157e4b1
TSP Team <tspteam.ordino@gmail.com>
2018-09-19 15:28:37 +0200

    second  commit
    added pref and login utils

---
+++ utils/src/main/java/com/tsp/logutils/Constants.java
@@ -0,0 +1,12 @@
+package com.tsp.logutils;
+/*
+ * Created by Shukri.
+ */
+
+public class Constants {
+    public final static String USERNAME_PREFERENCE = "username_preference";
+    public final static String PASSWORD_PREFERENCE = "password_preference";
+    public final static String TOKEN_PREFERENCE = "token_preference";
+    public final static String LANGUAGE_PREFERENCE = "language_preference";
+    public static final String ISLOGGEDIN = "ISLOGGEDIN";
+}
---
+++ utils/src/main/java/com/tsp/logutils/TspUtils.java
@@ -0,0 +1,142 @@
+package com.tsp.logutils;
+/*
+ * Created by Shukri.
+ */
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+
+public class TspUtils {
+//    public static boolean isNetworkAvailable(Context context) {
+//        ConnectivityManager manager = (ConnectivityManager) context
+//                .getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+//        boolean isAvailable = false;
+//        if (networkInfo != null && networkInfo.isConnected()) {
+//            isAvailable = true;
+//        }
+//        return isAvailable;
+//    }
+
+
+    public static void savePreferences(Context context, String key, long value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putLong(key, value);
+        editor.commit();
+    }
+
+    public static void savePreferences(Context context, String key, int value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    public static long getPreferences(Context context, String key, long defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getLong(key, defaultValue);
+    }
+
+
+    public static int getPreferences(Context context, String key, int defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getInt(key, defaultValue);
+    }
+
+    public static void savePreferences(Context context, String key, boolean value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    public static void savePreferences(Context context, String key, String value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public static boolean getPreferences(Context context, String key) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getBoolean(key, false);
+    }
+
+    public static boolean getPreferences(Context context, String key, boolean defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getBoolean(key, defaultValue);
+    }
+
+    public static String getPreferences(Context context, String key, String defaultV) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getString(key, defaultV);
+    }
+
+    public static void setFragmentTitle(Context context, String title) {
+        ((AppCompatActivity) context).getSupportActionBar().setTitle(title);
+    }
+
+    public static boolean isLoggedIn(Context context) {
+        return getPreferences(context, Constants.ISLOGGEDIN, false);
+    }
+
+    public static void logOut(Context context) {
+        savePreferences(context, Constants.ISLOGGEDIN, false);
+    }
+
+    public static void logIn(Context context, String username, String password) {
+        savePreferences(context, Constants.ISLOGGEDIN, true);
+        savePreferences(context, Constants.USERNAME_PREFERENCE, username);
+        savePreferences(context, Constants.PASSWORD_PREFERENCE, password);
+    }
+
+    public static void hideKeyboard(View view, final Context context) {
+        //Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard((Activity) context);
+                    return false;
+                }
+
+            });
+        }
+
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                hideKeyboard(innerView, context);
+            }
+        }
+    }
+
+    private static void hideSoftKeyboard(Activity activity) {
+        if (activity != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            if (activity.getCurrentFocus() != null)
+                inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+        } else {
+        }
+    }
+
+
+    public static String getMimeFromUri(Context context, String url) {
+        String type = null;
+        String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+        if (extension != null)
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+
+        return type;
+    }
+}
